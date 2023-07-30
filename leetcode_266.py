7521ed10a0bfd59b644d8643b24989f7fd9e888f
SungHwan, Ahn <43629468+ash1209@users.noreply.github.com>
2023-07-30 21:42:43 +0900

    Update leetcode_266.py

--- leetcode_266.py
+++ leetcode_266.py
@@ -1,5 +1,10 @@
 # 문제 : https://leetcode.com/problems/palindrome-permutation/description/?envType=study-plan-v2&envId=premium-algo-100
 
+# 문자열이 주어졌을 때, 문자를 재배치 해서 회문이 될 수 있는지를 판별하는 문제
+# Hash를 쓰면 너무 쉽게 풀리지만, 그건 너무 쉬워서 Hash 말고 다른 더 재미있는 방법이 없을까 고민하다가
+# Bit Masking을 사용해서도 풀 수 있지 않을까 해서 풀어본 문제
+
+
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
         bit_map = 0b0
